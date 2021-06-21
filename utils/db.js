a3b0a3513015e9271ecfdacf37899b0d1d423a1d
Rituparna Warwatkar <rituparnaw16@gmail.com>
2021-06-21 12:20:02 +0530

    :partying_face:grid

--- utils/db.js
+++ utils/db.js
@@ -4,6 +4,7 @@ const connectDB = () => {
   return mongoose.connect('mongodb://10.3.6.122:27017/raghav', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false
   });
 };
 
