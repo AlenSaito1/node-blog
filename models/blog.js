5d761d5b6e3e084f2de818483a16cf1872f3dc92
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-06-21 11:31:40 +0530

    update `blogSchema`

--- models/blog.js
+++ models/blog.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const marked = require('marked');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
 
 const blogSchema = new mongoose.Schema(
   {
@@ -14,10 +18,20 @@ const blogSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    sanitizedHtml: {
+      type: String,
+      required: true
+    }
   },
   {
     timestamps: true,
   }
 );
+
+
+blogSchema.pre('validate', function(next) {
+  if (this.content) this.sanitizedHtml = dompurify.sanitize(marked(this.content))
+  next()
+})
 
 module.exports = mongoose.model('Blog', blogSchema);
